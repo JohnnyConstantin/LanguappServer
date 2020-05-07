c34627ba165b1cba94ad6fc8954efa03437b3f4b
Krasav4ik <zkonst12@gmail.com>
2020-05-08 00:29:27 +0300

    Made project

---
+++ src/main/java/com/example/languappserver/LanguappserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.languappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguappserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LanguappserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/languappserver/LanguappserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.languappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguappserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
