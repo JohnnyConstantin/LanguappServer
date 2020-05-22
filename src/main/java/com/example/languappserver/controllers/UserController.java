562a8a112397c7f880b4c24bcb62542353ba99ec
Krasav4ik <zkonst12@gmail.com>
2020-05-22 20:46:44 +0300

    e

--- src/main/java/com/example/languappserver/controllers/UserController.java
+++ src/main/java/com/example/languappserver/controllers/UserController.java
@@ -26,15 +26,10 @@ public class UserController {
         return userService.getUser(id);
     }
 
-    @GetMapping("/data/{mail}")
-    public User getUser(@PathVariable String mail) {
-        return userService.getUserbyMail(mail);
-    }
-
     @RequestMapping(value = "/data", method = RequestMethod.POST)
     public String getMail(@RequestBody String mail){
         User user = userService.findUserById(mail);
-        return user.getName();
+        return user.getMail()+"/"+user.getPhone()+"/"+user.getName();
     }
 
     @RequestMapping(value = "/addUser", method = RequestMethod.POST)
